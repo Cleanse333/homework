824296ec304ad42e415da793a0e7128476fbe4eb
Cleanse333 <kokokhazix@gmail.com>
2025-08-25 17:14:44 +0400

    Add files via upload

---
+++ Homework2/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Koko Ebanoidze");
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine(); 
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
+
+
